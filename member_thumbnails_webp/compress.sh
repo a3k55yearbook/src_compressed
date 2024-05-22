3c9e1208390ec421f95e4f77abfe1ea8bf442b41
nhansp <43290699+nhansp@users.noreply.github.com>
2024-05-22 09:33:33 +0700

    update source

--- member_thumbnails_webp/compress.sh
+++ member_thumbnails_webp/compress.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 for image in $(cat './list.txt'); do
-    if [[ $(identify -format "%w" "$image") -ge 2000 || $(identify -format "%h" "$image") -ge 2000 ]]; then
-        convert "$image" -quality 95 -resize 1200 -sampling-factor 4:4:4 -filter Lanczos "${image::-4}.webp"
-    fi
+	convert "$image" -quality 85 -resize 1200 -sampling-factor 4:4:4 -filter Lanczos "${image::-4}.webp"
 done
